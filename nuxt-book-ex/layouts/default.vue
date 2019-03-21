94212164b5f78ff1c4924be2ef5e6c6d6a200b6d
QuangMinh <thanbathai@gmail.com>
2019-03-21 18:23:44 +0900

    first commit

---
+++ nuxt-book-ex/layouts/default.vue
@@ -0,0 +1,104 @@
+<template>
+  <v-app light>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list class="pa-1">
+        <v-list-tile v-if="miniVariant" @click.stop="miniVariant = !miniVariant">
+          <v-list-tile-action>
+            <v-icon>chevron_right</v-icon>
+          </v-list-tile-action>
+        </v-list-tile>
+
+        <v-list-tile avatar tag="div">
+          <v-list-tile-avatar>
+            <img src="https://randomuser.me/api/portraits/men/85.jpg">
+          </v-list-tile-avatar>
+
+          <v-list-tile-content>
+            <v-list-tile-title>John Leider</v-list-tile-title>
+          </v-list-tile-content>
+
+          <v-list-tile-action>
+            <v-btn icon @click.stop="miniVariant = !miniVariant">
+              <v-icon>chevron_left</v-icon>
+            </v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+      <v-list class="pt-0" dense>
+        <v-list-tile
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title" />
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      :clipped-left="clipped"
+      fixed
+      app
+      color="teal lighten-3"
+      dark
+      scroll-off-screen
+      scroll-target="#scrolling-techniques"
+    >
+      <v-toolbar-side-icon @click="drawer = !drawer" />
+      <v-toolbar-title v-text="title" />
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>notification_important</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>create</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>looks</v-icon>
+      </v-btn>
+      <v-avatar size="24px">
+        <img src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460">
+      </v-avatar>
+    </v-toolbar>
+    <nuxt/>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'bubble_chart',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      title: 'Ex-Book'
+    }
+  }
+}
+</script>
---
+++ nuxt-book-ex/pages/index.vue
@@ -0,0 +1,113 @@
+<template>
+  <v-app id="index-page">
+    <section>
+      <v-parallax src="https://github.com/vwxyzjn/vuetify-parallax-starter2/raw/master/src/assets/background1.jpg" height="450">
+        <v-layout column align-center justify-center>
+          <h2 class="teal--text text--lighten-2 mb-2">Parallax Template</h2>
+          <div class="white--text headline mb-3 text-xs-center">A modern responsive front-end framework based on Material Design</div>
+          <v-btn light large class="teal lighten-2 white--text" href="https://github.com/vwxyzjn/vuetify-parallax-starter">Get Started</v-btn>
+        </v-layout>
+      </v-parallax>
+    </section>
+    <div id="content">
+      <v-layout row wrap class="my-5" >
+        <v-flex xs12 sm4>
+          <v-card class="elevation-0">
+            <v-card-media height="45px" class="text-xs-center">
+              <v-icon x-large class="brown--text">flash_on</v-icon>
+            </v-card-media>
+            <v-card-title primary-title>
+              <div class="headline text-xs-center" style="width:100%">Speeds up development</div>
+            </v-card-title>
+            <v-card-text>
+              We did most of the heavy lifting for you to provide
+              a default stylings that incorporate our custom components.
+              Additionally, we refined animations and transitions to
+              provide a smoother experience for developers.
+            </v-card-text>
+          </v-card>
+        </v-flex>
+
+        <v-flex xs12 sm4>
+          <v-card class="elevation-0">
+            <v-card-media height="45px" class="text-xs-center">
+              <v-icon x-large class="brown--text">group</v-icon>
+            </v-card-media>
+            <v-card-title primary-title>
+              <div class="headline text-xs-center" style="width:100%">User Experience Focused</div>
+            </v-card-title>
+            <v-card-text>
+              By utilizing elements and principles of Material Design,
+              we were able to create a framework that incorporates components
+              and animations that provide more feedback to users. Additionally,
+              a single underlying responsive system across all platforms allow
+              for a more unified user experience.
+            </v-card-text>
+          </v-card>
+        </v-flex>
+
+        <v-flex xs12 sm4>
+          <v-card class="elevation-0">
+            <v-card-media height="45px" class="text-xs-center">
+              <v-icon x-large class="brown--text">settings</v-icon>
+            </v-card-media>
+            <v-card-title primary-title>
+              <div class="headline text-xs-center" style="width:100%">Easy to work with</div>
+            </v-card-title>
+            <v-card-text>
+              We have provided detailed documentation as well as specific code
+              examples to help new users get started. We are also always open
+              to feedback and can answer any questions a user may have about
+              Materialize.
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </div>
+    </section>
+
+    <section>
+      <v-parallax src="https://github.com/vwxyzjn/vuetify-parallax-starter2/raw/master/src/assets/background2.jpg" height="380">
+        <v-layout column align-center justify-center>
+          <div class="headline white--text text-xs-center">A modern responsive front-end framework based on Material Design</div>
+        </v-layout>
+      </v-parallax>
+    </section>
+
+    <section>
+      <div id="content">
+        <v-layout row wrap class="my-5" >
+          <v-flex xs12>
+            <v-card class="elevation-0">
+              <v-card-title primary-title>
+                <div class="headline text-xs-center" style="width:100%">Contact Us</div>
+              </v-card-title>
+              <v-card-text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat.
+                Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros.
+                Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere
+                et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl
+                maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida
+                ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in
+                faucibus orci luctus et ultrices posuere cubilia Curae;
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </div>
+    </section>
+    <v-content>
+      <v-container>
+
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    components: {}
+  }
+</script>
+<style>
+</style>
